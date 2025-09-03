fc10bb740b79ca1a3886e22e7acebb233eddfe0e
nkabembo <71999441+nkabembo@users.noreply.github.com>
2025-09-03 21:58:47 +0200

    added all currently installed apps in project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bucket_list',
+    'pages',
+    'users'
 ]
 
 MIDDLEWARE = [
