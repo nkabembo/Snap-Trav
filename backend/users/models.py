b792ee6ea3597c167be32a1d0e89c2d357f231e7
nkabembo <71999441+nkabembo@users.noreply.github.com>
2025-09-03 23:10:22 +0200

    Create UserAccount model, informed django user model in usage, Migrated UserAccount model and registered model with admin

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class UserAccount(AbstractUser):
+    #Other user fields username, email and password will come from AbstractUser since they are built-in
+    preferences = models.JSONField(blank=True,null=True)
+
+    def __str__(self):
+        return self.username
+
+
